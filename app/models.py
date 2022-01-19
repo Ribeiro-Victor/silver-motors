25843882086033b0bceed8b1b54d0847f4b62da3
Victor Ribeiro <ribeir145@gmail.com>
2022-01-19 11:01:59 -0300

    Commit inicial

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from .extensions import db
+
+class BaseModel(db.Model):
+    
+    __abstract__ = True
+
+    create_time = db.Column(db.String)
+    update_time = db.Column(db.String)
+
+    @staticmethod
+    def delete(obj):
+        db.session.delete(obj)
+        db.session.commit()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
